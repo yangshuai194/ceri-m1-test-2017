fc005b703a8b539b76751212b1f454528fa063d4
yangshuai <yangshuai194@gmail.com>
2018-04-18 13:47:43 +0200

    TP3 test files

--- src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/IGameStateProviderTest.java
@@ -6,19 +6,43 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import fr.univavignon.rodeo.api.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IGameStateProviderTest {
 	
+	public static IGameState gameState = IGameStateTest.getTestInstance();
+
 	protected static IGameStateProvider getTestInstance() throws IllegalStateException,IllegalArgumentException
 	{
 		IGameStateProvider gameStateProviderMock=Mockito.mock(IGameStateProvider.class);
-		IGameStateTest gameStateMock=Mockito.mock(IGameStateTest.class);
-
-		Mockito.doThrow(IllegalStateException.class).when(gameStateMock).testExploreArea();
-
+		Mockito.doThrow(IllegalArgumentException.class).when(gameStateProviderMock).get(null);
+		Mockito.when(gameStateProviderMock.get("gm")).thenReturn(gameState);
 		return gameStateProviderMock;
 	}
+
+	@Test
+	public void testSave()
+	{
+		final IGameStateProvider gameStateProvider=getTestInstance();
+		gameStateProvider.save(gameState);
+ 
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetNull()
+	{
+		final IGameStateProvider gameStateProvider=getTestInstance();
+		gameStateProvider.get(null);
+	}
+
+	@Test
+	public void testGet()
+	{
+		final IGameStateProvider gameStateProvider=getTestInstance();
+        assertEquals(gameStateProvider.get("gm"),gameState); 
+	}
+
+	
+	
 }
